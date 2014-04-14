52a460f49369482ab1401bd48f93cda93298cbee
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-04-14 18:03:29 +0300

    solve 15

--- NinetyNineProblems/ElevenToTwenty.hs
+++ NinetyNineProblems/ElevenToTwenty.hs
@@ -5,3 +5,7 @@ import NinetyNineProblems.OneToTen
 -- 14.)  Duplicate the elements of a list.
 dupli :: [b] -> [b]
 dupli = concatMap $ replicate 2
+
+-- 15.) Replicate the elements of a list a given number of times.
+repli :: (Enum t1, Num t1) => [t] -> t1 -> [t]
+repli xs n = [x | x <- xs, _ <-[1..n]]
--- test/NinetyNineProblems/ElevenToTwentySpec.hs
+++ test/NinetyNineProblems/ElevenToTwentySpec.hs
@@ -9,6 +9,11 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+
     describe "dupli" $ do
       it "should duplicate the elements of a list" $ do
         dupli [1, 2, 3] `shouldBe` [1, 1, 2, 2, 3, 3]
+
+    describe "repli" $ do
+      it "should replicate the elements of a list a given number of times" $ do
+        repli "abc" 3 `shouldBe` "aaabbbccc"
