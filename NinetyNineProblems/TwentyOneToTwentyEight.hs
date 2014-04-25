7aa7c3e878948724a67cd28f8c656c631bd75a5c
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-04-25 23:56:55 +0300

    solve 21, 22

--- NinetyNineProblems/TwentyOneToTwentyEight.hs
+++ NinetyNineProblems/TwentyOneToTwentyEight.hs
@@ -2,6 +2,10 @@ module NinetyNineProblems.TwentyOneToTwentyEight where
 
 
 -- 21.) Insert an element at a given position into a list.
+insertAt :: a -> [a] -> Int -> [a]
+insertAt el list index = take (index - 1) list ++ [el] ++ drop (index - 1) list
 
 
 -- 22.) Create a list containing all integers within a given range.
+range :: Enum t => t -> t -> [t]
+range a b = [x| x <- [a..b]]
